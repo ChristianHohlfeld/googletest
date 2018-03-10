c6742b0b78882d587240739f5e00cb922b10c03a
Christian Hohfleld <chris.hohlfeld@gmail.com>
2018-03-10 22:46:53 +0100

    initial commit

---
+++ GTest/GameTests/GameTests.cpp
@@ -0,0 +1,11 @@
+#include "gtest\gtest.h"
+#pragma once
+int square(const int value)
+{
+	return value * value;
+}
+TEST(SquareTests, Square)
+{
+	EXPECT_EQ(0, square(0));
+	EXPECT_EQ(4, square(-2));
+}
